904426f74888cf6bfa4362b443071228afcd80c9
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-09-03 19:24:25 +0700

    feat: add style Page not found

--- packages/client/src/pages/PageNotFound/PageNotFound.tsx
+++ packages/client/src/pages/PageNotFound/PageNotFound.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
-import { Page } from './StyledComponents'
+import { Page, Img, Title, SubTitle, LinkWrap } from './StyledComponents'
+import logo from '../../image/logo.svg'
 
 export const PageNotFound = () => {
-  return <Page>PageNotFound</Page>
+  return (
+    <Page>
+      <Img src={logo} alt="Логотип игры pacman" />
+      <Title>404</Title>
+      <SubTitle>Не туда попали</SubTitle>
+      <LinkWrap to="/">Назад</LinkWrap>
+    </Page>
+  )
 }
--- packages/client/src/pages/PageNotFound/StyledComponents.tsx
+++ packages/client/src/pages/PageNotFound/StyledComponents.tsx
@@ -1,5 +1,46 @@
 import styled from 'styled-components'
+import v from '../../Variables.module.scss'
+import { Link } from 'react-router-dom'
 
-const Page = styled.div``
+const Page = styled.div`
+  text-align: center;
+`
 
-export { Page }
+const Img = styled.img`
+  width: 130px;
+  height: 195px;
+  object-fit: cover;
+  object-position: center;
+  margin-bottom: 30px;
+`
+
+const Title = styled.h1`
+  margin: 0 0 20px 0;
+  font-size: 40px;
+  color: ${v.$black000};
+  font-weight: 500;
+  line-height: 40px;
+`
+
+const SubTitle = styled.p`
+  margin: 0 0 70px 0;
+  font-size: 20px;
+  color: ${v.$black000};
+  font-weight: 500;
+  line-height: 20px;
+`
+
+const LinkWrap = styled(Link)`
+  text-decoration: none;
+  font-size: 14px;
+  color: ${v.blue000};
+  font-weight: 500;
+  line-height: 11px;
+  transition: 0.3s opacity;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`
+
+export { Page, Img, Title, SubTitle, LinkWrap }
