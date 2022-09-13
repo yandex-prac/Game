5b01b8e69487a54be72fb8160011a11202561b56
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-09-14 02:54:05 +0700

    feat: add useIntl for leaderboard page

--- packages/client/src/pages/LeaderBoard/LeaderBoard.tsx
+++ packages/client/src/pages/LeaderBoard/LeaderBoard.tsx
@@ -1,15 +1,21 @@
 import React, { memo } from 'react'
 import { BaseLayout } from '@/components'
 import { Content, Title, LinkToGame } from './StyledComponents'
-import { PATHNAMES, CONTENT } from '@/utils'
+import { PATHNAMES } from '@/utils'
 import { DarkModeType } from '@/types'
+import { useIntl } from 'react-intl'
 
 export const LeaderBoard = memo(({ darkMode }: DarkModeType) => {
+  const intl = useIntl()
   return (
     <BaseLayout>
       <Content darkMode={darkMode}>
-        <Title darkMode={darkMode}>{CONTENT.LEADER_BOARD_TITLE}</Title>
-        <LinkToGame to={PATHNAMES.MAIN}>{CONTENT.LEADER_BOARD_LINK}</LinkToGame>
+        <Title darkMode={darkMode}>
+          {`${intl.messages.LEADER_BOARD_TITLE || ''}`}
+        </Title>
+        <LinkToGame to={PATHNAMES.MAIN}>
+          {`${intl.messages.LEADER_BOARD_LINK || ''}`}
+        </LinkToGame>
       </Content>
     </BaseLayout>
   )
