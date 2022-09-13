68a2e7b6456ff2d417b14e11c06b9a47f27d2036
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-09-14 02:55:39 +0700

    feat: add useIntl for SideBar component

--- packages/client/src/components/business/Sidebar/Sidebar.tsx
+++ packages/client/src/components/business/Sidebar/Sidebar.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { Container, Img } from './StyledComponents'
 import logo from '@/image/logo.svg'
 import { SidebarAvatar, Menu } from '@/components'
-import { CONTENT } from '@/utils'
 import { SidebarProps } from './types'
 import { DarkModeType } from '@/types'
+import { useIntl } from 'react-intl'
 
 export const Sidebar = ({
   darkMode,
   onChangeTheme,
 }: SidebarProps & DarkModeType) => {
+  const intl = useIntl()
   return (
     <Container darkMode={darkMode}>
-      <Img src={logo} alt={CONTENT.LOGO_ALT} />
+      <Img src={logo} alt={`${intl.messages.LOGO_ALT || ''}`} />
       <SidebarAvatar darkMode={darkMode} />
       <Menu darkMode={darkMode} onChangeTheme={onChangeTheme} />
     </Container>
