2e74375ad468eb94347b7a518cd926fe4c4b7412
Simaleon <striker151@gmail.com>
2022-09-15 19:32:36 +0300

    Исправление моргания игры

--- packages/client/src/game/Floor.ts
+++ packages/client/src/game/Floor.ts
@@ -1,14 +1,7 @@
 import { GameObject } from './GameObject'
-import wall from '@/image/gameSprites/floor.svg'
+import floor from '@/image/gameSprites/floor.svg'
 
 export class Floor extends GameObject {
   _isCollisional = false
-
-  get sprite(): HTMLImageElement {
-    const img = document.createElement('img')
-
-    img.src = wall
-
-    return img
-  }
+  _sprite = floor
 }
