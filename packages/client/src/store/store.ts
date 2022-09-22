df2e0697d0ff13445b8b168d133adea25ed29042
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-09-22 16:37:35 +0700

    feat: add middleware for redux saga

--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -1,6 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { authReducer } from './reducers'
 import { authAPI } from './services'
+import createSagaMiddleware from 'redux-saga'
+import { rootSaga } from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const rootStore = combineReducers({
   authReducer,
@@ -11,11 +15,13 @@ const setupStore = () => {
   return configureStore({
     reducer: rootStore,
     middleware: getDefaultMiddleware =>
-      getDefaultMiddleware().concat(authAPI.middleware),
+      getDefaultMiddleware().concat(sagaMiddleware).concat(authAPI.middleware),
   })
 }
 
 export default setupStore()
+
+sagaMiddleware.run(rootSaga)
 
 type RootState = ReturnType<typeof rootStore>
 type AppStore = ReturnType<typeof setupStore>
