2c8522fd8c7cdd87e180b859ec555b2825c24ac7
Leon <striker151@gmail.com>
2022-09-27 09:53:04 +0300

    Добавлен service worker (#25)
    Co-authored-by: Leonid Simakov <l.simakov@yadro.com>
    Co-authored-by: Evgeniy Karmyskov <karmyskove@gmail.com>

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 import path from 'path'
@@ -10,7 +11,15 @@ export default defineConfig({
   server: {
     port: Number(process.env.CLIENT_PORT) || 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
