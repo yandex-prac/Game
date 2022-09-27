1529e3a335a7421518b638b723d4bec2428aecf5
Leon <striker151@gmail.com>
2022-09-27 19:16:19 +0300

    Добавлена игра (#17)
    * Начало работы над игрой
    
    * Пачка доработок по функционалу игры
    
    * Пачка доработок по функционалу игры
    
    * Добавлены точки и конец игры
    
    * Исправление моргания игры
    
    * Финальные доработки игры
    
    * Исправления по результатам PR
    
    * Фикс неотображения кнопки начала и зависимостей
    
    * Исправление ошибок
    
    Co-authored-by: Leonid Simakov <l.simakov@yadro.com>

--- packages/client/src/pages/Main/Main.tsx
+++ packages/client/src/pages/Main/Main.tsx
@@ -1,10 +1,52 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useRef, useState } from 'react'
 import { BaseLayout } from '@/components'
+import { Game } from '@/game'
+import { CanvasWrapper, StartButton } from './StyledComponents'
 
 export const Main = memo(() => {
+  const canvas = useRef<HTMLCanvasElement>(null)
+  const [startTime, setStartTime] = useState(0)
+  const [endTime, setEndTime] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  let game: Game | undefined
+
+  useEffect(() => {
+    if (!game) {
+      game = new Game(canvas.current as HTMLCanvasElement)
+
+      game.on('endGame', () => {
+        setEndTime(Date.now())
+        setIsPlaying(false)
+      })
+    }
+  }, [])
+
   return (
     <BaseLayout>
-      <p>LeaderBoard</p>
+      <CanvasWrapper>
+        {isPlaying ? null : (
+          <h1>
+            Ваше время:
+            {new Date(endTime - startTime).toLocaleTimeString([], {
+              second: 'numeric',
+              minute: 'numeric',
+            })}
+          </h1>
+        )}
+        <canvas width={400} height={400} ref={canvas} />
+        {isPlaying ? null : (
+          <StartButton
+            type="button"
+            textIntl="PLAY_GAME_MENU_ITEM"
+            onClick={() => {
+              setIsPlaying(true)
+              game?.start()
+              setStartTime(Date.now())
+            }}
+          />
+        )}
+      </CanvasWrapper>
     </BaseLayout>
   )
 })
