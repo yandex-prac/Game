79b787223379167bcbe59d3a40c90ececd54b615
webInfrastructure <66786645+webInfrastructure@users.noreply.github.com>
2022-09-27 13:48:47 +0700

    Добавлен ErrorBoundary. В качестве примера повешен на … (#23)
    * feat: add profile service
    
    * feat: add types for getUserById
    
    * feat: add changeUserProfile end point
    
    * feat: add changeUserAvatar end point
    
    * feat: add changeUserPassowrd end point
    
    * refactor: decomposed code
    
    * Первый коммит. Добавлен ErrorBoundary. В качестве примера повешен на чат.
    
    * hotfix
    
    * pr hotfix
    
    * refactor: change path for type
    
    Co-authored-by: Evgeniy Karmyskov <karmyskove@gmail.com>
    Co-authored-by: Konstantin Rolgaizer <atlant1995@yandex.ru>

---
+++ packages/client/src/components/business/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ErrorFallbackType } from './types'
+
+export function ErrorFallback({
+  error,
+  resetErrorBoundary,
+}: ErrorFallbackType) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
