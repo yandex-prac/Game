0b7da0ba87bc9d50048cc426094efa988597c7f8
Konstantin Rolgaizer <atlant1995@yandex.ru>
2022-10-02 22:36:13 +0300

    аудио апи, в процессе

---
+++ packages/client/src/components/ui/AudioButton/AudoiButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Button } from '@/components'
+import useFullscreenStatus from '@/hooks/useFullscreenStatus'
+
+export const FullscreenButton = () => {
+  const root = document.getElementById('root')
+  const [isFullscreen, setIsFullscreen] = useFullscreenStatus(root)
+  const handleFullscreen = () => {
+    // Ругается TS, непонятно на что
+    if (typeof setIsFullscreen === 'boolean') {
+      return
+    }
+    const handleExitFullscreen = () => document.exitFullscreen()
+    if (isFullscreen) {
+      handleExitFullscreen()
+    } else {
+      setIsFullscreen()
+    }
+  }
+  return (
+    <Button
+      type="button"
+      textIntl={
+        isFullscreen ? 'FULLSCREEN_MENU_ITEM_EXIT' : 'FULLSCREEN_MENU_ITEM'
+      }
+      onClick={handleFullscreen}
+    />
+  )
+}
---
+++ packages/client/src/components/ui/AudioButton/index.ts
@@ -0,0 +1,1 @@
+export { FullscreenButton } from './FullscreenButton'
