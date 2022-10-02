1763235e470b2b6187ea03343ce5ea508aa9a638
eframeoff <63045531+eframeoff@users.noreply.github.com>
2022-10-02 22:34:53 +0300

    feat: add tests (#27)

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,5 +1,16 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'],
+  testMatch: ['<rootDir>/src/**/*.test.tsx'],
+  moduleNameMapper: {
+    '^.+\\.(css|scss|svg|jpg)$': 'babel-jest',
+    '^@/components(.*)$': '<rootDir>/src/components$1',
+    '^@/pages(.*)$': '<rootDir>/src/pages$1',
+    '^@/utils(.*)$': '<rootDir>/src/utils$1',
+    '^@/store(.*)$': '<rootDir>/src/store$1',
+    '^@/context(.*)$': '<rootDir>/src/context$1',
+    '^@/hooks(.*)$': '<rootDir>/src/hooks$1',
+    '^@/image(.*)$': '<rootDir>/src/image$1',
+    '^@/game(.*)$': '<rootDir>/src/game$1',
+  },
 }
