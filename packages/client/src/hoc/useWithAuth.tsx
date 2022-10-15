c2029f228587e3518bcf37c05a614197befc2d6e
eframeoff <vacra@mail.ru>
2022-10-15 16:57:35 +0300

    fix: fix auth hoc

--- packages/client/src/hoc/useWithAuth.tsx
+++ packages/client/src/hoc/useWithAuth.tsx
@@ -3,32 +3,35 @@ import { useNavigate } from 'react-router-dom'
 import { PATHNAMES, CONTENT_RU } from '@/utils'
 import { useLazyGetUserInfoQuery } from '@/store'
 
-export const WithAuth =
-  (Component: any) => (props: any) => {
-    const navigate = useNavigate()
-    const [getUser, { isSuccess }] = useLazyGetUserInfoQuery()
+export const WithAuth = (Component: any) => (props: any) => {
+  const navigate = useNavigate()
+  const [getUser, { isSuccess }] = useLazyGetUserInfoQuery()
 
-    useEffect(() => {
-      if (!localStorage.getItem(CONTENT_RU.IS_LOGIN_IN)) {
-        getUser(undefined)
-      }
-    }, [isSuccess])
+  useEffect(() => {
+    if (!localStorage.getItem(CONTENT_RU.IS_LOGIN_IN)) {
+      getUser(undefined)
+    }
+  }, [])
 
-    const isLogout =
-      !localStorage.getItem(CONTENT_RU.IS_LOGIN_IN) &&
-      window.location.pathname !== PATHNAMES.SIGNIN &&
-      window.location.pathname !== PATHNAMES.SIGNUP
+  const isLogout =
+    !localStorage.getItem(CONTENT_RU.IS_LOGIN_IN) &&
+    window.location.pathname !== PATHNAMES.SIGNIN &&
+    window.location.pathname !== PATHNAMES.SIGNUP
 
-    const isLogin =
-      localStorage.getItem(CONTENT_RU.IS_LOGIN_IN) &&
-      !isSuccess &&
-      (window.location.pathname == PATHNAMES.SIGNIN ||
-        window.location.pathname == PATHNAMES.SIGNUP)
+  const isLogin =
+    localStorage.getItem(CONTENT_RU.IS_LOGIN_IN) &&
+    !isSuccess &&
+    (window.location.pathname == PATHNAMES.SIGNIN ||
+      window.location.pathname == PATHNAMES.SIGNUP)
 
-    useEffect(() => {
-      isLogout && navigate(PATHNAMES.SIGNIN)
-      isLogin && navigate(PATHNAMES.MAIN)
-    }, [])
+  useEffect(() => {
+    isLogout && navigate(PATHNAMES.SIGNIN)
+    isLogin && navigate(PATHNAMES.MAIN)
+  }, [])
 
-    return <Component {...props} />
+  if (isLogout || isLogin) {
+    return <> </>
   }
+
+  return <Component {...props} />
+}
