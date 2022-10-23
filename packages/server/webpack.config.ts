71e11ad78a5889927b14dacab7a596d65520ff45
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-10-24 05:34:26 +0700

    chore: добавлены alias для путей

--- packages/server/webpack.config.ts
+++ packages/server/webpack.config.ts
@@ -1,34 +1,37 @@
-import type { Configuration } from 'webpack';
-import path from 'path';
+import type { Configuration } from 'webpack'
+import path from 'path'
 
-const DIST_DIR = path.join(__dirname, 'dist');
+const DIST_DIR = path.join(__dirname, 'dist')
 
 const config: Configuration = {
   target: 'node',
   entry: './index.tsx',
   module: {
     rules: [
-  //     {
-  //       test: /\.(html)$/,
-  //       use: 'null-loader',
-  //     },
-  //     {
-  //       test: /\.(png|jpe?g|gif|mp3|svg|webp|ico)$/i,
-  //       type: 'asset',
-  //     },
-  //     {
-  //       test: /\.(woff2?|eot|ttf|otf)$/i,
-  //       type: 'asset/resource',
-  //     },
+      //     {
+      //       test: /\.(html)$/,
+      //       use: 'null-loader',
+      //     },
+      //     {
+      //       test: /\.(png|jpe?g|gif|mp3|svg|webp|ico)$/i,
+      //       type: 'asset',
+      //     },
+      //     {
+      //       test: /\.(woff2?|eot|ttf|otf)$/i,
+      //       type: 'asset/resource',
+      //     },
       {
         test: /\.(ts|tsx)$/,
         exclude: /(node_modules)/,
-        use: { loader: 'babel-loader', options: { presets: ['@babel/env','@babel/preset-react'] }, },
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/env', '@babel/preset-react'] },
+        },
       },
-  //     {
-  //       test: /\.(s[ac]|c)ss$/i,
-  //       use: 'null-loader',
-  //     },
+      //     {
+      //       test: /\.(s[ac]|c)ss$/i,
+      //       use: 'null-loader',
+      //     },
     ],
   },
   output: {
@@ -43,7 +46,10 @@ const config: Configuration = {
     //     configFile: './tsconfig.json',
     //   }),
     // ],
+    alias: {
+      '@': path.resolve(__dirname, '../client/src'),
+    },
   },
-};
+}
 
-export default config;
+export default config
