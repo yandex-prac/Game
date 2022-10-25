b62c0fa3dc4f065e6e8970a166843abe55ec0b3d
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-10-25 22:39:10 +0700

    chore: добавен compression

--- packages/server/index.tsx
+++ packages/server/index.tsx
@@ -1,12 +1,15 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
 import express from 'express'
+import compression from 'compression'
 import { renderTemplate } from './middlewares'
 
 dotenv.config()
 
 const app = express()
-app.use(cors())
+
+app.use(cors()).use(compression())
+
 const port = Number(process.env.SERVER_PORT) || 3001
 
 app.get('/*', renderTemplate)
