8cc83ff2811fdfdb82884c7a55079f5b0b43e0c1
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-10-25 17:11:34 +0700

    chore: добавлен style-loader

--- packages/server/webpack.config.ts
+++ packages/server/webpack.config.ts
@@ -30,14 +30,7 @@ const config: Configuration = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
+        use: ['style-loader', 'sass-loader'],
       },
       {
         test: /\.(ts|tsx)$/,
