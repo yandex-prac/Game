68c1fc797558cd3bafd77206d89a29a8a802a22b
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-10-25 21:03:47 +0700

    refactor: декомпозиция логики

--- packages/server/index.tsx
+++ packages/server/index.tsx
@@ -1,20 +1,15 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
-import { renderToString } from 'react-dom/server'
-dotenv.config()
-import React from 'react'
-import { App } from 'client/src/components/business/App'
-
 import express from 'express'
+import { renderTemplate } from './middlewares'
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
 const port = Number(process.env.SERVER_PORT) || 3001
 
-app.get('/', (_, res) => {
-  // res.json('👋 Howdy from the server :)')
-  res.send(renderToString(<App />))
-})
+app.get('/*', renderTemplate)
 
 app.listen(port, () => {
   console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
