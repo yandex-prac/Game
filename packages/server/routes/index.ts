2c6d83ee3f2d2808b345c2ca5d2c5c95a4437b35
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-10-31 20:29:14 +0700

    chore: чистка кода

--- packages/server/routes/index.ts
+++ packages/server/routes/index.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express'
 import userRouter from './userRouter'
-// import topicRouter from './topicRouter'
-// import commentRouter from './commentRouter'
 
 const router: Router = Router()
 
 router.use('/user', userRouter)
-// router.use(`${API_URL}/topic`, topicRouter)
-// router.use(`${API_URL}/comment`, commentRouter)
 
 export default router
