89c4e3a0fd928b1e24b43dd9787c2c5216b18677
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-11-01 02:52:15 +0700

    fix: исправлен баг с закрытием по клику вне зоны

--- packages/client/src/components/ui/Modal/Modal.tsx
+++ packages/client/src/components/ui/Modal/Modal.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { ModalType } from './types'
 import { Wrapper, Inner, Title } from './StyledComponents'
 
 export const Modal = ({ children, isOpen, onCloseModal, title }: ModalType) => {
+  const modalRef = useRef(null)
+
+  const handleCloseByOutZone = (evt: Event) => {
+    const element = modalRef.current as null | Element
+    const target = evt.target as HTMLElement
+    if (!isOpen) return
+    if (element && !element.contains(target)) {
+      onCloseModal()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleCloseByOutZone, true)
+
+    return () =>
+      document.removeEventListener('click', handleCloseByOutZone, true)
+  }, [isOpen])
+
   return (
-    <Wrapper isOpen={isOpen} onClick={onCloseModal}>
-      <Inner>
+    <Wrapper isOpen={isOpen}>
+      <Inner ref={modalRef}>
         <Title>{title}</Title>
         {children}
       </Inner>
