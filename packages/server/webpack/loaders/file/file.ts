776826807cd06ed3a0f4ad6e82558455b8671632
eframeoff <vacra@mail.ru>
2022-11-01 18:11:08 +0300

    fix: format

--- packages/server/webpack/loaders/file/file.ts
+++ packages/server/webpack/loaders/file/file.ts
@@ -1,18 +1,18 @@
 export default {
-    client: {
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-    },
-    server: {
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-    },
-  }
+  client: {
+    test: /\.(png|jpe?g|gif|svg)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  },
+  server: {
+    test: /\.(png|jpe?g|gif|svg)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  },
+}
--- packages/server/webpack/loaders/ts/ts.ts
+++ packages/server/webpack/loaders/ts/ts.ts
@@ -1,30 +1,30 @@
 export default {
-    client: {
-      test: /\.(ts|tsx)$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/env',
-            '@babel/preset-react',
-            '@babel/preset-typescript',
-          ],
-        },
+  client: {
+    test: /\.(ts|tsx)$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          '@babel/env',
+          '@babel/preset-react',
+          '@babel/preset-typescript',
+        ],
       },
     },
-    server: {
-      test: /\.(ts|tsx)$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/env',
-            '@babel/preset-react',
-            '@babel/preset-typescript',
-          ],
-        },
+  },
+  server: {
+    test: /\.(ts|tsx)$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          '@babel/env',
+          '@babel/preset-react',
+          '@babel/preset-typescript',
+        ],
       },
     },
-  }
+  },
+}
