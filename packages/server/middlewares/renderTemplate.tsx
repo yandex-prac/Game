8ce77e0f8357c019bec79bb15774e71b8494c5fa
Konstantin Rolgaizer <atlant1995@yandex.ru>
2022-11-01 15:27:47 +0100

    fixes

--- packages/server/middlewares/renderTemplate.tsx
+++ packages/server/middlewares/renderTemplate.tsx
@@ -4,48 +4,50 @@ import { IntlProvider } from 'react-intl'
 import { Helmet, HelmetData } from 'react-helmet'
 import type { Request, Response } from 'express'
 import { CONTENT_RU } from 'client/src/utils'
-import { App } from 'client/src/components/business/App'
+import { store } from 'client/src/store'
+import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router-dom/server'
-// import { DataStaticRouterContext } from 'react-router'
+import { App } from 'client/src/components/business/App'
 
-// import { store } from 'client/src/store'
-// import { Provider } from 'react-redux'
-
-const getHtml = (reactHtml: string, helmetData: HelmetData) => {
+const getHtml = (
+  reactHtml: string,
+  helmetData: HelmetData,
+  reduxState = {}
+) => {
   return `
     <!DOCTYPE html>
-    <html lang="ru">
+    <html lang='ru'>
     <head>
-      <meta charset="UTF-8" />
-      <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charset='UTF-8' />
+      <link rel='icon' type='image/svg+xml' href='/vite.svg' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       ${helmetData.title.toString()}
       ${helmetData.meta.toString()}
     </head>
     <body>
-      <div id="root">${reactHtml}</div>
-      <script src="/main.js"></script>
+      <div id='root'>${reactHtml}</div>
+      <script>
+        window.__INITIAL_STATE__ = ${JSON.stringify(reduxState)}
+      </script>
+      <script src='/main.js'></script>
     </body>
     </html>
     `
 }
 
 export const renderTemplate = (_req: Request, res: Response) => {
-  // console.log('SSR store', store)
   const location = _req.url
-  // const context: any = {}
-  // console.log('SSR context', context)
   const jsx = (
-    // <Provider store={store}>
-    <IntlProvider messages={CONTENT_RU} locale="ru" defaultLocale="ru">
-      <StaticRouter location={location}>
-        <App />
-      </StaticRouter>
-    </IntlProvider>
-    // </Provider>
+    <Provider store={store}>
+      <IntlProvider messages={CONTENT_RU} locale="ru" defaultLocale="ru">
+        <StaticRouter location={location}>
+          <App />
+        </StaticRouter>
+      </IntlProvider>
+    </Provider>
   )
   const reactHtml = renderToString(jsx)
   const helmetData = Helmet.renderStatic()
-
-  res.send(getHtml(reactHtml, helmetData))
+  const reduxState = store.getState()
+  res.send(getHtml(reactHtml, helmetData, reduxState))
 }
